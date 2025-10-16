92b835811e8e3e80812eb152f6a1ad9443e3ddaa
absolutegoaat <santi_icr@outlook.com>
2025-10-16 16:21:17 +0000

    add further documentation, add route to grab from specific person

--- api/README.md
+++ api/README.md
@@ -1,7 +1,8 @@
 # CipherAPI
 This api works by interacting the database by API and not by mysql credentials.
 
-sorry if im not detailed.
+sorry if im not detailed, if there's any issues about the api don't hesitate to 
+submit an issue on github! :D
 
 ![ralsei](https://c.tenor.com/a67qt4MuklQAAAAM/ralsei-dancing-ralsei.gif)
 
@@ -33,3 +34,15 @@ if __name__ == "__main__":
 ```
 
 Obviously you can change the ```User-Agent``` but yea 
+
+# /api/people
+```Method: GET```
+
+As in the python example on the variable ```BASE``` on the authentication part, 
+you'll be able to get everyone on the database.
+
+# /api/people/{id}
+```Method: GET```
+
+You can pull specific people from the database instead of 
+grabbing the entire database.
--- api/fetchpeople.py
+++ api/fetchpeople.py
@@ -3,16 +3,25 @@ from database import DatabaseManager
 from api.auth import require_api_key # makes auth for the API
 
 people_bp = Blueprint("people", __name__)
+db = DatabaseManager()
+
+# TODO: Validation Route
 
 @people_bp.route("/api/people", methods=["GET"])
 @require_api_key
 def get_people():
-    db = DatabaseManager()
     people = db.get_all_people()
     
     return jsonify(people)
 
 @people_bp.route("/api/people/<int:person_id>")
 @require_api_key
-def get_person():
-    print() #placeholder
+def get_person(person_id):
+    person = db.get_person(person_id=person)
+
+    if person:
+        return jsonify(person)
+    else
+        return jsonify({
+            'message': 'Person is not found in database.'
+        }), 404
